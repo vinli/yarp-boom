41e07383229b132643e7c8a4b75ca9c972da5305
Greg Aker <greg@gregaker.net>
2018-01-03 23:16:28 -0600

    Bump boom version

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const yarp = require('yarp');
 const onError = (err) => {
   /* istanbul ignore else */
   if (err.statusCode && err.data) {
-    throw Boom.create(err.statusCode, err.data.message);
+    throw new Boom(err.data.message, { statusCode: err.statusCode });
   } else {
     throw err;
   }
